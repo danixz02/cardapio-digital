65750a5b23ca54312f7b672814cab5407cb407e7
Hanakoo <danielmobile098@gmail.com>
2025-02-06 06:17:48 -0300

    reset files

--- script.js
+++ script.js
@@ -18,34 +18,33 @@ function closeMenu() {
 }
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Função para carregar o arquivo JSON
-  fetch("cardapio.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Exibir os lanches
-      const lanchesSection = document.getElementById("listaLanches");
-      const templateCard = document.querySelector(".card.template");
-
-      data.cardapio.lanches.forEach((lanche) => {
-        
-        const lancheCard = templateCard.cloneNode(true);
-        lancheCard.classList.remove("template"); // Remove a classe que oculta o card
-        lancheCard.style.display = ""; // Mostra o card
-
-        // Preenche as informações no card
-        lancheCard.querySelector(".cardImg").src = lanche.imagem;
-        lancheCard.querySelector(".cardImg").alt = lanche.nome;
-        lancheCard.querySelector(".cardTitle").textContent = lanche.nome;
-       /*  lancheCard.querySelector(".cardIngredientes").innerHTML = `Ingredientes: <br>
-        ${lanche.ingredientes.join(", ")}`;
-        lancheCard.querySelector(".cardValor").textContent = lanche.valor; */
-
-        // Adiciona o card preenchido na lista
-        lanchesSection.appendChild(lancheCard);
+    // Função para carregar o arquivo JSON
+    fetch("cardapio.json")
+      .then((response) => response.json())
+      .then((data) => {
+        // Exibir os lanches
+        const lanchesSection = document.getElementById("listaLanches");
+        const templateCard = document.querySelector(".card.template");
+  
+        data.cardapio.lanches.forEach((lanches) => {
+          
+          const lanchesCard = templateCard.cloneNode(true);
+          lanchesCard.classList.remove("template"); // Remove a classe que oculta o card
+          lanchesCard.style.display = ""; // Mostra o card
+  
+          // Preenche as informações no card
+          lanchesCard.querySelector(".cardImg").src = lanches.imagem;
+          lanchesCard.querySelector(".cardImg").alt = lanches.nome;
+          lanchesCard.querySelector(".cardTitle").textContent = lanches.nome;
+  
+          // Adiciona o card preenchido na lista
+          lanchesSection.appendChild(lanchesCard);
+        });
+      })
+      .catch((error) => {
+        console.log("Erro ao carregar o arquivo JSON:", error);
       });
-    })
-    .catch((error) => {
-      console.log("Erro ao carregar o arquivo JSON:", error);
-    });
-});
+  });
+  
